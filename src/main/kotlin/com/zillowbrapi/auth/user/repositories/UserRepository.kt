223efb4b2732c627f5b0acdc4dd9a8b9b13a230d
Geazi Oliveira <geazioliveira@gmail.com>
2025-09-06 22:33:32 -0300

    Refactor `UserRepository` package structure; relocate from `auth.user` to `auth.user.repositories`.

--- src/main/kotlin/com/zillowbrapi/auth/user/repositories/UserRepository.kt
+++ src/main/kotlin/com/zillowbrapi/auth/user/repositories/UserRepository.kt
@@ -1,6 +1,6 @@
-package com.zillowbrapi.auth.user
+package com.zillowbrapi.auth.user.repositories
 
-import com.zillowbrapi.auth.user.model.UserEntity
+import com.zillowbrapi.auth.user.models.UserEntity
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
