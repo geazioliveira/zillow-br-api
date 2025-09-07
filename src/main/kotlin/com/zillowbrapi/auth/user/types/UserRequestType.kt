f34fd61f80c863387a53efe1681887618eba58ec
Geazi Oliveira <geazioliveira@gmail.com>
2025-09-06 22:34:37 -0300

    Fix `UserEntity` import path; update from `model` to `models` for consistency and correctness.

--- src/main/kotlin/com/zillowbrapi/auth/user/types/UserRequestType.kt
+++ src/main/kotlin/com/zillowbrapi/auth/user/types/UserRequestType.kt
@@ -2,7 +2,7 @@ package com.zillowbrapi.auth.user.types
 
 import com.zillowbrapi.auth.user.dtos.UserCreateRequest
 import com.zillowbrapi.auth.user.dtos.UserUpdateRequest
-import com.zillowbrapi.auth.user.model.UserEntity
+import com.zillowbrapi.auth.user.models.UserEntity
 
 sealed class UserRequestType {
     data class Create(val request: UserCreateRequest) : UserRequestType()
