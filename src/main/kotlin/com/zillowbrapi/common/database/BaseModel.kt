c46738913140b191f4f0092e42c40b50f11c8c11
Geazi Oliveira <geazioliveira@gmail.com>
2025-09-07 15:38:02 -0300

    Update `BaseModel` to use `UUID` for `id` field instead of `Long`.

--- src/main/kotlin/com/zillowbrapi/common/database/BaseModel.kt
+++ src/main/kotlin/com/zillowbrapi/common/database/BaseModel.kt
@@ -1,9 +1,10 @@
 package com.zillowbrapi.common.database
 
 import java.time.Instant
+import java.util.*
 
 interface BaseModel {
-    val id: Long?
+    val id: UUID?
     val createdAt: Instant?
     val updatedAt: Instant?
     val deletedAt: Instant?
