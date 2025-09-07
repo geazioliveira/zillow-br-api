a59ff448ff6bb754f0f3b5e3705b95b3c5c759fc
Geazi Oliveira <geazioliveira@gmail.com>
2025-09-07 15:38:33 -0300

    Update `UserRepository` to use `UUID` for `id` instead of `String`.

--- src/main/kotlin/com/zillowbrapi/auth/user/repositories/UserRepository.kt
+++ src/main/kotlin/com/zillowbrapi/auth/user/repositories/UserRepository.kt
@@ -3,9 +3,10 @@ package com.zillowbrapi.auth.user.repositories
 import com.zillowbrapi.auth.user.models.UserEntity
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
+import java.util.*
 
 @Repository
-interface   UserRepository : JpaRepository<UserEntity, String> {
+interface   UserRepository : JpaRepository<UserEntity, UUID> {
     fun existsByEmail(email: String): Boolean
     fun findByEmail(email: String): UserEntity?
 }
