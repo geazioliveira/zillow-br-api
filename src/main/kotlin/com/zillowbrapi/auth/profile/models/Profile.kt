ef4fe4c94dda21ab0e5a0e2491366862da6b1934
Geazi Oliveira <geazioliveira@gmail.com>
2025-09-07 23:37:08 -0300

    Add `Profile` interface and update `ProfileEntity` with factory methods and default values.

---
+++ src/main/kotlin/com/zillowbrapi/auth/profile/models/Profile.kt
@@ -0,0 +1,19 @@
+package com.zillowbrapi.auth.profile.models
+
+import com.zillowbrapi.auth.profile.types.ProfileType
+import com.zillowbrapi.auth.profile.types.Visibility
+import com.zillowbrapi.auth.user.models.UserEntity
+import com.zillowbrapi.common.database.BaseModel
+import java.util.*
+
+interface Profile: BaseModel {
+    val user: UserEntity?
+    val userId: UUID?
+    val type: ProfileType?
+    val avatarUrl: String?
+    val coverUrl: String?
+    val bio: String?
+    val primaryCity: String?
+    val primaryState: String?
+    val visibility: Visibility?
+}
--- src/main/kotlin/com/zillowbrapi/auth/profile/models/ProfileEntity.kt
+++ src/main/kotlin/com/zillowbrapi/auth/profile/models/ProfileEntity.kt
@@ -14,15 +14,34 @@ import jakarta.persistence.*
 class ProfileEntity (
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id", nullable = false)
-    var user: UserEntity,
+    var user: UserEntity? = null,
 
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
-    var type: ProfileType,
+    var type: ProfileType = ProfileType.CONSUMER,
     var avatarUrl: String? = null,
     var coverUrl: String? = null,
+
+    @Column(columnDefinition = "Text")
     var bio: String? = null,
     var primaryCity: String? = null,
     var primaryState: String? = null,
     var visibility: Visibility = Visibility.PRIVATE
-) : BaseEntity()
+) : BaseEntity() {
+    constructor(profile: Profile) : this(
+        user = profile.user ?: throw IllegalArgumentException("User cannot be null"),
+        type = profile.type ?: ProfileType.CONSUMER,
+        avatarUrl = profile.avatarUrl,
+        coverUrl = profile.coverUrl,
+        bio = profile.bio,
+        primaryCity = profile.primaryCity,
+        primaryState = profile.primaryState,
+        visibility = profile.visibility ?: Visibility.PRIVATE
+    )
+
+    companion object {
+        fun from(profile: Profile): ProfileEntity {
+            return ProfileEntity(profile)
+        }
+    }
+}
