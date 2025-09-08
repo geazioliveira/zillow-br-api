d902fb5983818530014404ff07fbcfb4050c3878
Geazi Oliveira <geazioliveira@gmail.com>
2025-09-07 23:38:04 -0300

    Update `ProfileRepository` to use `UUID` for `id` instead of `String`.

--- src/main/kotlin/com/zillowbrapi/auth/profile/repositories/ProfileRepository.kt
+++ src/main/kotlin/com/zillowbrapi/auth/profile/repositories/ProfileRepository.kt
@@ -3,6 +3,7 @@ package com.zillowbrapi.auth.profile.repositories
 import com.zillowbrapi.auth.profile.models.ProfileEntity
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
+import java.util.*
 
 @Repository
-interface ProfileRepository : JpaRepository<ProfileEntity, String>
+interface ProfileRepository : JpaRepository<ProfileEntity, UUID>
