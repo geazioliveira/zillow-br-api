3b0c102afb4a719b565a30d88fc53cf4507632d5
Geazi Oliveira <geazioliveira@gmail.com>
2025-09-08 11:50:33 -0300

    Add JPA repository for `AgentProfileEntity` with custom query methods

--- src/main/kotlin/com/zillowbrapi/auth/profile/repositories/AgentProfileRepository.kt
+++ src/main/kotlin/com/zillowbrapi/auth/profile/repositories/AgentProfileRepository.kt
@@ -1,3 +1,20 @@
 package com.zillowbrapi.auth.profile.repositories
 
-class AgentProfileRepository
+import com.zillowbrapi.auth.profile.models.AgentProfileEntity
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import java.util.*
+
+interface AgentProfileRepository : JpaRepository<AgentProfileEntity, UUID> {
+    fun existsByCreciNumber(creciNumber: String): Boolean
+    fun findByProfileId(profileId: UUID): AgentProfileEntity?
+    @Query("SELECT a FROM AgentProfileEntity a JOIN FETCH a.profile WHERE a.id = :id")
+    fun findByIdWithProfile(id: UUID): AgentProfileEntity?
+
+    @Query("SELECT a FROM AgentProfileEntity a JOIN FETCH a.profile p WHERE p.user.id = :userId")
+    fun findByUserId(userId: UUID): AgentProfileEntity?
+
+    fun existsByCreciNumberAndCreciUf(creciNumber: String, creciUf: String): Boolean
+
+    fun findByCreciNumber(creciNumber: String): AgentProfileEntity
+}
