bd1855feef4e4502906fcb67874ec7bbf771d880
Geazi Oliveira <geazioliveira@gmail.com>
2025-09-12 19:49:40 -0300

    Update import paths in `ProfileRequestType` to align with package structure changes

--- src/main/kotlin/com/zillowbrapi/auth/profile/types/ProfileRequestType.kt
+++ src/main/kotlin/com/zillowbrapi/auth/profile/types/ProfileRequestType.kt
@@ -1,8 +1,8 @@
 package com.zillowbrapi.auth.profile.types
 
-import com.zillowbrapi.auth.profile.dtos.ProfileCreateRequest
-import com.zillowbrapi.auth.profile.dtos.ProfileUpdateRequest
-import com.zillowbrapi.auth.profile.models.ProfileEntity
+import com.zillowbrapi.auth.profile.models.entities.ProfileEntity
+import com.zillowbrapi.auth.profile.validators.ProfileCreateRequest
+import com.zillowbrapi.auth.profile.validators.ProfileUpdateRequest
 
 sealed class ProfileRequestType {
     data class CreateProfile(val request: ProfileCreateRequest) : ProfileRequestType()
